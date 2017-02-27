9a814ae152b3447e382da922966bb5ab33e7450d
bcmcpher <bcmcpher@iu.edu>
2017-02-27 02:34:00 -0500

    incremental improvements to files

--- feConnectomeSummary.m
+++ feConnectomeSummary.m
@@ -55,12 +55,12 @@ fibLength = fefgGet(fg, 'length');
 %
 % the rest of the cleaning is in plot_path_neighborhood
 
-% compute offset between label images space and fe image space
-offset = aparc_img2acpc - fe.life.xform.img2acpc;
-offset = offset(1:3, 4);
-offset = offset';
-%offset = round(offset') + 1;
-%offset = [offset(1) offset(2) offset(3)];
+% % compute offset between label images space and fe image space
+% offset = aparc_img2acpc - fe.life.xform.img2acpc;
+% offset = offset(1:3, 4);
+% offset = offset';
+% %offset = round(offset') + 1;
+% %offset = [offset(1) offset(2) offset(3)];
 
 % initialize endpoint outputs
 ep1 = zeros(length(fg.fibers), 3);
@@ -92,9 +92,9 @@ ep2 = round(ep2) + 1;
 
 %% assign fiber endpoints to labels
 
-% pull fe.roi.coords and convert to acpc coords - do I even need this?
-%fe_roi = mrAnatXformCoords(fe.life.xform.img2acpc, fe.roi.coords);
-%fe_roi = round(fe_roi) + 1;
+% % pull fe.roi.coords and convert to acpc coords - do I even need this?
+% fe_roi = mrAnatXformCoords(fe.life.xform.img2acpc, fe.roi.coords);
+% fe_roi = round(fe_roi) + 1;
 
 % pull all labels (non-zero)
 labels = unique(aparc.data);
@@ -105,6 +105,7 @@ display(['Matching streamlines to ' num2str(length(labels)) ' nodes...']);
 % preallocate outputs
 out = cell(length(labels), 1);
 tfib = zeros(length(labels), 1);
+%wfib = zeros(length(labels), 1);
 
 % for every label, assign endpoints
 tic;
@@ -123,7 +124,7 @@ for ii = 1:length(labels)
     acpcCoords = round(acpcCoords) + 1;
    
     % catch size 
-    out{ii}.size = size(acpcCoords, 1);
+    out{ii}.size = size(unique(acpcCoords, 'rows'), 1);
     
     % test - other out - do I find more streamlines w/o limiting to fe_roi intersection?
     % just find intersections of all fiber end points directly w/ ROIs -
@@ -132,7 +133,8 @@ for ii = 1:length(labels)
     roi_ep2 = ismember(ep2, acpcCoords, 'rows');
     
     % for fibers that end in rois, catch indices / lengths / weights 
-    out{ii}.end.fibers = [ find(roi_ep1); find(roi_ep2) ];
+    fibers = [ find(roi_ep1); find(roi_ep2) ];
+    out{ii}.end.fibers = fibers;
     out{ii}.end.length = fibLength(out{ii}.end.fibers);
     out{ii}.end.weight = fe.life.fit.weights(out{ii}.end.fibers);
     
@@ -144,57 +146,65 @@ for ii = 1:length(labels)
 %  %  % this finds fewer, and if wm rois inflate correctly the steps below are irrelevant
 %  %  % don't need tensor to align ROIs - unless node order is encoded it's
 %  %  % not worth dealing with tensor to assign fibers.
-%
+% 
 %     % find label intersection w/ wm mask
 %     roi_wmi = ismember(fe_roi, acpcCoords, 'rows');
 %     
 %     if sum(roi_wmi) <= 0
 %         warning(['ROI label ' num2str(labels(ii)) ' never intersects the white matter.']);
-%         out{ii}.fibers.int = [];
-%         out{ii}.fibers.end = [];
+%         out{ii}.feroi.int = [];
+%         out{ii}.feroi.end = [];
 %         continue
 %     end
 %     
-%     % subset tensor to label voxels for fiber indices
-%     [ inds, ~ ] = find(fe.life.M.Phi(:, find(roi_wmi), :));
+%     % label roi indicies
+%     roi_ind = find(roi_wmi);
 %     
+%     % subset tensor to label voxels for fiber indices
+%     [ inds, ~ ] = find(fe.life.M.Phi(:, roi_ind, :));
+%     
+%     % if inds is not a sufficiently sized input below, go to next ROI
 %     if size(inds, 2) < 3
 %         warning(['The found streamline dimensions in label ' num2str(labels(ii)) ' are wrong, none can be identified.']);
-%         out{ii}.fibers.int = [];
-%         out{ii}.fibers.end = [];
+%         out{ii}.feroi.int = [];
+%         out{ii}.feroi.end = [];
 %         continue
 %     end
 %     
 %     % fiber indices of ROI
-%     out{ii}.fibers.int = unique(inds(:, 3));
+%     out{ii}.feroi.int = unique(inds(:, 3));
 %     
 %     % determine if endpoint is in ROI
 %     
 %     % subset streamline endpoints to fibers that intersect ROI
-%     roi_ep1 = ep1(out{ii}.fibers.int, :);
-%     roi_ep2 = ep2(out{ii}.fibers.int, :);
+%     feroi_ep1 = ep1(out{ii}.feroi.int, :);
+%     feroi_ep2 = ep2(out{ii}.feroi.int, :);
 %     
 %     % for every fiber that intersects, determine if it ends in the label
-%     frst = ismember(roi_ep1, acpcCoords, 'rows');
-%     last = ismember(roi_ep2, acpcCoords, 'rows');
+%     frst = ismember(feroi_ep1, acpcCoords, 'rows');
+%     last = ismember(feroi_ep2, acpcCoords, 'rows');
 %             
 %     % catch and merge the indices of the end points
-%     out{ii}.fibers.end = [ out{ii}.fibers.int(frst); out{ii}.fibers.int(last) ];
+%     out{ii}.feroi.end = [ out{ii}.feroi.int(frst); out{ii}.feroi.int(last) ];
 %     
-%     if isempty(out{ii}.fibers.end)
+%     if isempty(out{ii}.feroi.end)
 %         warning(['No streamlines terminate in ROI ' num2str(labels(ii)) '. Label voxels intersect white matter ROI in ' num2str(sum(roi_wmi)) ' voxels.']);
 %     end
     
     % total fibers assigned to an endpoint
     tfib(ii) = length(out{ii}.end.fibers);
+    %wfib(ii) = length(out{ii}.feroi.end);
     
 end
 time = toc; 
 
 display(['Successfully assigned ' num2str(sum(tfib)) ' of ' num2str(2*size(fe.life.M.Phi, 3)) ' endpoints in ' num2str(round(time)) ' seconds.']);
-clear ii x y z imgCoords acpcCoords roi_ep1 roi_ep2 time
+%display(['Successfully assigned ' num2str(sum(wfib)) ' of ' num2str(2*size(fe.life.M.Phi, 3)) ' endpoints in ' num2str(round(time)) ' seconds.']);
 
-%% build cound of endpoint values
+clear ii x y z imgCoords acpcCoords roi_ep1 roi_ep2 fibers time
+%clear roi_wmi inds feroi_ep1 feroi_ep2 frst last
+
+%% build  paired connections object
 
 % build every unique combination of labels
 pairs = nchoosek(1:length(labels), 2);
@@ -214,11 +224,17 @@ for ii = 1:length(pairs)
     pconn{ii}.roi1 = roi1.label;
     pconn{ii}.roi2 = roi2.label;
     
+    % catch size of ROIs
+    pconn{ii}.roi1sz = roi1.size;
+    pconn{ii}.roi2sz = roi2.size;
+    
     % assign intersections of terminating streamlines
-    pconn{ii}.end = intersect(roi1.end.fibers, roi2.end.fibers);
-   
+    pconn{ii}.indices = intersect(roi1.end.fibers, roi2.end.fibers);
+    pconn{ii}.lengths = fibLength(pconn{ii}.indices);
+    pconn{ii}.weights = fe.life.fit.weights(pconn{ii}.indices);
+    
     % keep running total of total streamlines assigned a connection
-    tcon(ii) = size(pconn{ii}.end, 1);
+    tcon(ii) = size(pconn{ii}.indices, 1);
     
 end
 
@@ -226,19 +242,57 @@ display(['Build paired connections object with ' num2str(sum(tcon)) ' fibers.'])
 
 clear ii roi1 roi2 
 
+%% create cleaned version of each connection
+
+% parfor ii = 1:length(pconn)
+
+%% run parallelized virtural lesion
+
+% parfor ii = 1:length(pconn)
+
+%% build tract profiles
+
+% for ii = 1:length(ms_files)
+%     parfor jj = 1:length(pconn)
+
 %% build matrices from pconn
 
 display('Building Adjacency Matrices...');
 
 % initialize output
-emat = zeros(length(labels));
+emat = zeros(length(labels), length(labels), 4);
 
 % for every paired connection
 for ii = 1:length(pconn)
-   
-    % assign pconn values into adjacency matrix
-    emat(pairs(ii, 1), pairs(ii, 2)) = size(pconn{ii}.end, 1);
-    emat(pairs(ii, 2), pairs(ii, 1)) = size(pconn{ii}.end, 1);
+    
+    % values that can be reused - count, combined voxel size, average length
+    cnt = size(pconn{ii}.indices, 1);
+    psz = pconn{ii}.roi1sz + pconn{ii}.roi2sz;
+    len = mean(pconn{ii}.lengths);
+    
+    % 1. count of streamlines
+    emat(pairs(ii, 1), pairs(ii, 2), 1) = cnt;
+    emat(pairs(ii, 2), pairs(ii, 1), 1) = cnt;
+    pconn{ii}.matrix.count = cnt;
+    
+    % 2. density of streamlines
+    dns = (2 * cnt) / psz;
+    emat(pairs(ii, 1), pairs(ii, 2), 2) = dns;
+    emat(pairs(ii, 2), pairs(ii, 1), 2) = dns;
+    pconn{ii}.matrix.density = dns;
+    
+    % 3. length of streamlines
+    emat(pairs(ii, 1), pairs(ii, 2), 3) = len;
+    emat(pairs(ii, 2), pairs(ii, 1), 3) = len;
+    pconn{ii}.matrix.length = len;
+    
+    % 4. density controlling for length (Hagmann, 2008)
+    dln = (2 / psz) * sum(1 / pconn{ii}.lengths);
+    emat(pairs(ii, 1), pairs(ii, 2), 4) = dln;
+    emat(pairs(ii, 2), pairs(ii, 1), 4) = dln;
+    pconn{ii}.matrix.dnleng = dln;
+    
+    % add non-zero weighted counts
     
 end
 
@@ -246,7 +300,30 @@ clear ii
 
 end
 
-%% how many endpoints get found in all ROI labels
+%% check for duplicates
+
+% % for every combination of edges
+% comb = nchoosek(1:length(pconn), 2);
+% 
+% % for every connection
+% for ii = 1:length(comb)
+%     % the intersection of fiber indices should be empty. if they are not
+%     % empty, that's bad. If it's bad, print a warning.
+%     bad = ~isempty(intersect(pconn{comb(ii, 1)}.end, pconn{comb(ii, 2)}.end));
+%     if bad
+%         warning(['Fibers counted in 2 connections. comb index: ' num2str(ii)]);
+%     end
+% end
+% % this should never print a warning if things are working correctly
+
+
+%% debug plotting
+
+% excellent...
+%tmpfg = fgCreate('name', '', 'fibers', {fe.fg.fibers{pconn{962}.end}}');
+%bsc_quickPlot(tmpfg, [ 0 0 1 ]);
+
+%% how many endpoints get found in all ROI labels - became enpoint heatmat plot
 
 % % find and convert all ROI labels to acpc space
 % [ x1, y1, z1 ] = ind2sub(size(aparc.data), find(aparc.data > 0));
@@ -293,26 +370,3 @@ end
 % axis equal; axis square;
 % view(0, 0);
 % clear ii
-
-%% check for duplicates
-
-% % for every combination of edges
-% comb = nchoosek(1:length(pconn), 2);
-% 
-% % for every connection
-% for ii = 1:length(comb)
-%     % the intersection of fiber indices should be empty. if they are not
-%     % empty, that's bad. If it's bad, print a warning.
-%     bad = ~isempty(intersect(pconn{comb(ii, 1)}.end, pconn{comb(ii, 2)}.end));
-%     if bad
-%         warning(['Fibers counted in 2 connections. comb index: ' num2str(ii)]);
-%     end
-% end
-% % this should never print a warning if things are working correctly
-
-
-%% debug plotting
-
-% excellent...
-%tmpfg = fgCreate('name', '', 'fibers', {fe.fg.fibers{pconn{962}.end}}');
-%bsc_quickPlot(tmpfg, [ 0 0 1 ]);
