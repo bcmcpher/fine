297d46f2f8408700642b8f9e9d76f8c01248cb87
bcmcpher <bcmcpher@iu.edu>
2017-03-31 17:45:51 -0400

    updated render fxn to display multiple brain slices if requested

--- renders/render_connection.m
+++ renders/render_connection.m
@@ -1,4 +1,4 @@
-function [ fh, fg ] = render_connection(fe, pconn, tract, persp, slice, label, conn, pnprc)
+function [ fh, fgOut ] = render_connection(fe, pconn, label, tract, persp, slice, conn)
 % render_connection() renders an ENCODE object with network connection structure  
 %   Using a fit FE fiber group and pconn indexes, this fxn renders a
 %   connection, path neighborhood, or both with or without cleaning using MBA.
@@ -14,15 +14,13 @@ function [ fh, fg ] = render_connection(fe, pconn, tract, persp, slice, label, c
 % load the anatomy from fe
 anatomy = niftiRead(fe.path.anatomy);
 
-% path to save down figures
-%savedir = './figs/';
-
 %% other arguments...
 
-% add default if pnprc is empty / undefined...
+% percent of path neighborhood to display
+pnprc = 0.025;
 
 % set minimum length of 10mm
-threshold_length = 10;
+threshold_length = 15;
 
 % generate the blue / red colors for the tracts
 colors = {[.1 .25 .65], [.75 .25 .1]};
@@ -38,121 +36,107 @@ display('Converting streamlines to ACPC space...');
 fg = feGet(fe, 'fg acpc');
 
 % create fiber group object for connection
-fg_tract = fgCreate('name', fgName, 'colorRgb', colors{1}, 'fibers', fg.fibers(getfield(pconn{tract}, label, 'indices')));
 ind1 = getfield(pconn{tract}, label, 'indices');
-
-% % pick tracts to plot
-% switch cleaned
-%     case{'all'}
-%         fg_tract = fgCreate('name', fgName, 'colorRgb', colors{1}, 'fibers', {fe.fg.fibers{pconn{tract}.end.fibers}}');
-%         ind1 = pconn{tract}.end.fibers;
-%     case{'nzfibs'}
-%         fg_tract = fgCreate('name', fgName, 'colorRgb', colors{1}, 'fibers', {fe.fg.fibers{pconn{tract}.end.nzfibs}}');
-%         ind1 = pconn{tract}.end.nzfibs;
-%     case{'cln-all'}
-%         fg_tract = fgCreate('name', fgName, 'colorRgb', colors{1}, 'fibers', {fe.fg.fibers{pconn{tract}.cln.fibers}}');
-%         ind1 = pconn{tract}.cln.fibers;
-%     case{'cln-nzf'}       
-%         fg_tract = fgCreate('name', fgName, 'colorRgb', colors{1}, 'fibers', {fe.fg.fibers{pconn{tract}.cln.nzfibs}}');
-%         ind1 = pconn{tract}.cln.nzfibs;
-%     otherwise
-%         error(['Invalid type of fibers selected: ' cleaned ' is not an option. Please check you input for the ''clean'' argument.']);
-% end
-% 
-display(['Rednering ' fgName '...']);
 
 % if an emtpy fg is requested
 if isempty(ind1)
     error('The connection requested contains no streamlines at this parameter.');
 end
 
-% pull path neighborhood indices
-ind2 = feGet(fe, 'Path Neighborhood', ind1);
+% create path fiber group
+fgOut{1} = fgCreate('name', fgName, 'colorRgb', colors{1}, 'fibers', fg.fibers(ind1));
 
-% % create fiber group
-% fg = feGet(fe, 'fibers img');
-% 
-% % diff order of extract / align
-% xform = feGet(fe, 'img2acpcxform');
-% fg_xform = dtiXformFiberCoords(fg, xform, 'acpc');
+%% render figure
 
-% subset path neighborhood from whole brain fg, clear whole brain fg
-fg_pathn = fgExtract(fg, ind2, 'keep');
+display(['Rednering ' fgName '...']);
+
+% if I need the path neighborhood
+switch conn
+    case{'path'}
+        
+        display('Rendering path...');
+        
+    case{'neighborhood', 'both'}
+        
+        display('Rending path neighborhood...');
+        
+        % pull path neighborhood indices
+        ind2 = feGet(fe, 'Path Neighborhood', ind1);
+        
+        % subset path neighborhood from whole brain fg
+        fgOut{2} = fgExtract(fg, ind2, 'keep');
+        
+        % filter out short path neighborhood streamlines
+        c = 1;
+        fibers = cell(1, length(fgOut{2}.fibers));
+        for ii = 1:length(fgOut{2}.fibers)
+            if length(fgOut{2}.fibers{ii}) > threshold_length
+                fibers{c} = fgOut{2}.fibers{ii};
+                c = c + 1;
+            end
+        end
+        
+        fgOut{2}.fibers = fibers;
+        clear fibers
+        
+        % pull a random subset of the path neigborhood to minimize the rendering
+        fibs_indx = randsample(1:length(fgOut{2}.fibers), round(length(fgOut{2}.fibers) * pnprc));
+        fgOut{2}.fibers = fgOut{2}.fibers(fibs_indx);
+        
+    otherwise
+        error('Invalid connection type requested. Can render either: ''path'', ''neighborhood'', or ''both''.');
+        
+end
 
 clear fg
 
-% transform edge coords to acpc space, add to catch in output object
-%fg{1} = dtiXformFiberCoords(fg_tract, xform, 'acpc');
-fg{1} = fg_tract;
-
-% drop unused field and create final path neighborhood object before filtering
-%fg_pathn = rmfield(fg_pathn, 'coordspace');
-fg_pnplot = fg_pathn;
-
-% SHOULD BE DONE AS PART OF CLEANING IN CONNECTOMES, NOT PLOTTING
-% filter fiber lenghts in path neighborhood
-c = 1;
-fibers = cell(1, length(fg_pathn.fibers));
-for ii = 1:length(fg_pathn.fibers)
-    if length(fg_pathn.fibers{ii}) > threshold_length
-        fibers{c} = fg_pathn.fibers{ii};
-        c = c + 1;
-    end
-end
-
-fg_pathn.fibers = fibers; 
-clear fibers
-
-% pull a random subset of the path neigborhood to minimize the rendering
-fibs_indx = randsample(1:length(fg_pathn.fibers), round(length(fg_pathn.fibers) * pnprc));
-fg_pnplot.fibers = fg_pnplot.fibers(fibs_indx);
-
-% assign path neighborhood as second 
-fg{2} = fg_pnplot;
-
 %% plot the requested connection / view / fg
 
-% switch statement to define the plots perspective
-switch persp
-    case{'lh-sag'}
-        viewCoords = [-90, 0];
-        slice = [slice 0 0];
-    case{'rh-sag'}
-        viewCoords = [90, 0];
-        slice = [slice 0 0];
-    case{'axial'}
-        viewCoords = [0, 90];
-        slice = [0 0 slice];
-    case{'coronal'}
-        viewCoords = [180, 0];
-        slice = [0 slice 0];
+for ii = 1:length(persp)
+    
+    tpersp = persp{ii};
+    
+    % switch statement to define the plots perspective
+    switch tpersp
+        case{'lh-sag'}
+            viewCoords = [-90, 0];
+            indx = [slice(ii) 0 0];
+        case{'rh-sag'}
+            viewCoords = [90, 0];
+            indx = [slice(ii) 0 0];
+        case{'axial'}
+            viewCoords = [0, 90];
+            indx = [0 0 slice(ii)];
+        case{'coronal'}
+            viewCoords = [180, 0];
+            indx = [0 slice(ii) 0];
+        otherwise
+            error('Invalid slice view requested.');
+    end
+    
+    % plot the brain slice
+    fh = mbaDisplayBrainSlice(anatomy, indx, gca); hold on;
+    
 end
-
-% plot the brain slice
-fh = mbaDisplayBrainSlice(anatomy, slice, gca); hold on;
 
 % switch statement for which connections to model
 switch conn
     case{'path'}
-        [fh, light_h] = mbaDisplayConnectome(fg{1}.fibers, fh, colors{1}, 'single');
+        [fh, light_h] = mbaDisplayConnectome(fgOut{1}.fibers, fh, colors{1}, 'single');
         delete(light_h);
     case{'neighborhood'}
-        [fh, light_h] = mbaDisplayConnectome(fg{2}.fibers, fh, colors{2}, 'single');
+        [fh, light_h] = mbaDisplayConnectome(fgOut{2}.fibers, fh, colors{2}, 'single');
         delete(light_h);
     case{'both'}
-        [fh, light_h1] = mbaDisplayConnectome(fg{1}.fibers, fh, colors{1}, 'single');
+        [fh, light_h1] = mbaDisplayConnectome(fgOut{1}.fibers, fh, colors{1}, 'single');
         delete(light_h1);
-        [fh, light_h2] = mbaDisplayConnectome(fg{2}.fibers, fh, colors{2}, 'single');
-        delete(light_h2);
+        [fh, light_h2] = mbaDisplayConnectome(fgOut{2}.fibers, fh, colors{2}, 'single');
+        delete(light_h2);      
 end
 
 % plot this way every time
 view(viewCoords);
 camlight right
 lighting phong
-
-% save images? - don't run, this breaks the session
-%feSavefig(fhNum, 'verbose', 'yes', 'figName', fig.names{iview}, 'figDir', fullFigureOutDir, 'figType', 'jpg');
-%print(fh, '-cmyk', '-painters', '-depsc2', '-tiff', '-r500' , '-noui', './renders/test.eps');
 
 end
