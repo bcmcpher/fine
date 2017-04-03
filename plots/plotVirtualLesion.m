d118164c2fabaca3c81c6e4d46be09531c919e13
bcmcpher <bcmcpher@iu.edu>
2017-04-03 17:24:51 -0400

    removed plot from inflate labels; added new plotting / network scripts

---
+++ plots/plotVirtualLesion.m
@@ -0,0 +1,90 @@
+function [ fig ] = plotVirtualLesion(se)
+%plotVirtualLesion recreates the distributions from the LiFE paper for any
+% virtual lesion.
+%   
+
+%% raw RMSE distirbutions
+fig(1).name = 'rmse_distributions';
+fig(1).h    = figure('name', fig(1).name, 'color', 'w');
+fig(1).type = 'eps';
+set(fig(1).h,'Units','normalized','Position',[0.007 0.55  0.28 0.36]);
+plot(se.lesion.xhist,se.lesion.hist,'-','color', [.95 .45 .1],'linewidth',2); hold on
+plot(se.nolesion.xhist,se.nolesion.hist,'-','linewidth',2, 'color', [.1 .45 .95])
+plot([se.nolesion.rmse.mean,se.nolesion.rmse.mean], [0,0.12],'-','color',[.1 .45 .95] )
+plot([se.lesion.rmse.mean,se.lesion.rmse.mean], [0,0.12], '-', 'color',[.95 .45 .1])
+title(sprintf('mean RMSE\nno-lesion %2.3f | lesion %2.2f',se.nolesion.rmse.mean,se.lesion.rmse.mean),'fontsize',16)
+ylabel('Probability', 'fontsize',14);xlabel('RMSE', 'fontsize',14)
+legend({'Lesion','No lesion'},'fontsize',14);
+set(gca,'box','off','xtick',[0 round(se.xrange(2)/2) se.xrange(2)],'ytick',[0 .06 .12],'xlim',[0 se.xrange(2)],'ylim',[0 .125], ...
+    'tickdir', 'out', 'ticklength', [0.025 0])
+
+%% bootstraps from strength of evidence
+
+ywo_e = se.s.lesioned_e;
+y_e   = se.s.unlesioned_e;
+woxhis = se.s.lesioned.xbins;
+xhis   = se.s.unlesioned.xbins;
+min_x = se.s.min_x;
+max_x = se.s.max_x;
+
+% find maximum of plot
+pmax = max(max([y_e(:), ywo_e(:)]));
+ran = linspace(0, pmax, 3);
+
+fig(2).name = 'virtual_lesion_test_mean_rmse_hist';
+fig(2).h    = figure('name', fig(2).name, 'color', 'w');
+fig(2).type = 'eps';
+set(fig(2).h, 'Units', 'normalized', 'Position', [0.007 0.55  0.28 0.36]);
+%patch([xhis, xhis], y_e(:), [.1 .45 .95], 'FaceColor', [.1 .45 .95], 'EdgeColor', [.1 .45 .95]); hold on
+line(xhis, y_e(:, 1)); hold on
+line(xhis, y_e(:, 2));
+fill(xhis, y_e(:, 2), [.1 .45 .95]);
+fill(xhis, y_e(:, 1), 'w');
+%patch([woxhis, woxhis], ywo_e(:), [.95 .45 .1], 'FaceColor', [.95 .45 .1], 'EdgeColor', [.95 .45 .1]);
+line(woxhis, ywo_e(:, 1));
+line(woxhis, ywo_e(:, 2));
+fill(woxhis, ywo_e(:, 2), [.95 .45 .1]);
+fill(woxhis, ywo_e(:, 1), 'w');
+
+%'ylim',[ran(1) ran(3)], ...
+%'ytick', ran, ...
+set(gca,'tickdir','out', ...
+    'box','off', ...
+    'ylim',[0 0.25], ...
+    'xlim',[min_x, max_x], ...
+    'ytick',[0 0.1 0.2], ...
+    'xtick',round(linspace(min_x, max_x, 4)), ...
+    'fontsize', 16)
+ylabel('Probability', 'fontsize', 16)
+xlabel('rmse', 'fontsize', 16)
+title(sprintf('Strength of connection evidence %2.3f',(se.s.mean)), 'FontSize', 16);
+
+%% all VL computed measures
+
+fig(3).name = 'Size_of_effect_of_the_lesion';
+fig(3).h    = figure('name',fig(3).name,'color','w');
+set(fig(3).h,'Units','normalized','Position',[0.007 0.55  0.28 0.36]);
+subplot(1,4,1)
+plot(1,se.s.mean,'-o','color', [.95 .45 .1],'linewidth',2); hold on
+plot([1,1], [se.s.mean,se.s.mean] + [-se.s.std,se.s.std], '-','color',[.95 .45 .1] )
+ylabel('S (s.d.)', 'fontsize',14);
+set(gca,'box','off','xlim',[0 2], 'ylim',[0 ceil(se.s.mean + se.s.std)], ...
+    'tickdir', 'out', 'ticklength', [0.025 0])
+subplot(1,4,2)
+plot(1,se.em.mean,'-o','color', [.95 .45 .1],'linewidth',2); hold on
+ylabel('Earth mover''s distance (raw scanner units)', 'fontsize',14);
+set(gca,'box','off','xlim',[0 2], 'ylim',[0 ceil(se.em.mean)], ...
+    'tickdir', 'out', 'ticklength', [0.025 0])
+subplot(1,4,3)
+plot(1,se.kl.mean,'-o','color', [.95 .45 .1],'linewidth',2); hold on
+ylabel('K-L divergence (bits)', 'fontsize',14);
+set(gca,'box','off','xlim',[0 2], 'ylim',[0 max(se.kl.mean)], ...
+    'tickdir', 'out', 'ticklength', [0.025 0])
+subplot(1,4,4)
+plot(1,se.j.mean,'-o','color', [.95 .45 .1],'linewidth',2); hold on
+ylabel('Jeffrey''s divergence (bits)', 'fontsize',14);
+set(gca,'box','off','xlim',[0 2], 'ylim',[0 max(se.j.mean)], ...
+    'tickdir', 'out', 'ticklength', [0.025 0])
+
+end
+
---
+++ scripts/fnBuildDKNetworks.m
@@ -0,0 +1,171 @@
+function [ pconn, rois, omat, olab, files ] = fnBuildDKNetworks(subj, track, lmax, rep, nclust, cacheDir)
+%fnBuildShen268Networks creates all the data I need for the paper from a fe structure.
+%   This will probably get renamed because this is a short, simple process
+%   to run that is canonical with many pipelines.
+%
+% INPUTS: all inputs are strings
+% subj  - subject ID, matches folder name
+% track - tracking parameter, 'SD_STREAM', 'SD_PROB', 'tensor_'
+% lmax  - lmax parameter
+% rep   - repetition number
+%
+% subj = '105115'; track = 'SD_PROB'; lmax='10'; rep='01'; nclust = 12; cacheDir='/N/dc2/projects/lifebid/lifeconn/matlab/cache';
+% [ pconn, rois, omat, olab, files ] = fnBuildShen268Networks('105115', 'SD_PROB', '10', '01', 12, '/N/dc2/projects/lifebid/lifeconn/matlab/cache');
+%
+
+%% build paths
+
+% build file names if tensor is called
+if strcmp(track, {'tensor_'})
+    files.fe = ['fe_structure_' subj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
+    files.out     = ['fn_' subj '_aparc+aseg_' track '_rep' rep '.mat'];
+    files.csv     = ['fn_' subj '_aparc+aseg_' track '_rep' rep ];
+else
+    files.fe = ['fe_structure_' subj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
+    files.out     = ['fn_' subj '_aparc+aseg_' track '_lmax' lmax '_rep' rep '.mat'];
+    files.csv     = ['fn_' subj '_aparc+aseg_' track '_lmax' lmax '_rep' rep ];
+end
+
+% build paths files to save
+files.fedir  = '/N/dc2/projects/lifebid/code/ccaiafa/Caiafa_Pestilli_paper2015/Revision_Feb2017/Results/Single_TC/fe_structures';
+files.path    = '/N/dc2/projects/lifebid/lifeconn';
+files.favol   = fullfile(files.path, 'subjects', subj, 'micro', 'fa.nii.gz');
+files.outpath = fullfile(files.path, 'subjects', subj, 'networks');
+files.output  = fullfile(files.outpath, files.out);
+files.labels  = fullfile(files.path, 'subjects', subj, 'anat', 'aparc+aseg_inflated_3_vert.nii.gz');
+
+% conditionals to build subject names, because that's not annoying
+
+% data sets with funny names somewhere in the process
+
+% because these FE file names are still different by data set...
+
+STN = {'FP', 'KK', 'HT', 'MP'};
+HC7 = {'7t_108323', '7t_109123', '7t_131217', '7t_910241'};
+
+% stanford data has this extra file info after subject ID in the fe structure names, not paths
+if any(strcmp(subj, STN))
+    nsubj = [ subj '_96dirs_b2000_1p5iso' ];
+    files.fe = ['fe_structure_' nsubj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
+end
+
+% 7T subjects are 7t_*, except in the fe structure names, where they're just the number
+if any(strcmp(subj, HC7))
+    nsubj = strrep(subj, '7t_', '');
+    files.fe = ['fe_structure_' nsubj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
+end
+
+clear STN HC7
+
+%% load files
+
+display('Loading data...');
+
+% load fe structure
+load(fullfile(files.fedir, subj, files.fe));
+
+% extract all needed out of FE
+fg               = feGet(fe,   'fg acpc'); 
+fascicle_length  = fefgGet(fg, 'length');
+fascicle_weights = feGet(fe,   'fiber weights');
+nTheta           = feGet(fe,   'nbvals');
+M                = feGet(fe,   'model');
+measured_dsig    = feGet(fe,   'dsigdemeaned by voxel');
+
+clear fe
+
+% load labeled aparc+aseg volume
+parc = niftiRead(files.labels);
+
+%% start parallel pool
+
+display(['Opening parallel pool with ', num2str(nclust), ' cores...']);
+
+% create parallel cluster object
+clust = parcluster;
+
+% set number of cores from arguments
+clust.NumWorkers = nclust;
+
+% set temporary cache directory
+tmpdir = tempname(cacheDir);
+
+% make cache dir
+OK = mkdir(tmpdir);
+
+% check and set cachedir location
+if OK
+    % set local storage for parpool
+    clust.JobStorageLocation = tmpdir;
+end
+
+% start parpool - close parpool at end of fxn
+pool = parpool(clust, nclust, 'IdleTimeout', 120);
+
+clear tmpdir OK
+
+%clear subj track lmax rep nclust cacheDir
+
+%% create networks
+
+% assign streamline endpoints to labeled volume
+[ pconn, rois ] = feCreatePairedConnections(parc, fg.fibers, fascicle_length, fascicle_weights);
+
+% workspace cleaning
+save(files.output, 'rois');
+clear parc rois
+
+% clean networks
+pconn = feCleanPairedConnections(fg, pconn, 'all');
+pconn = feCleanPairedConnections(fg, pconn, 'nzw');
+
+% run virtual lesion
+pconn = feVirtualLesionPairedConnections(M, fascicle_weights, measured_dsig, nTheta, pconn, 'nzw');
+pconn = feVirtualLesionPairedConnections(M, fascicle_weights, measured_dsig, nTheta, pconn, 'nzw_clean');
+
+% compute tract profiles
+favol = niftiRead(files.favol);
+
+pconn = feTractProfilePairedConnections(fg, pconn, 'nzw', favol, 'fa');
+pconn = feTractProfilePairedConnections(fg, pconn, 'nzw_clean', favol, 'fa');
+
+clear favol
+
+%% create adjacency matrices
+
+% create all streamline matrices
+[ amat, alab ] = feCreateAdjacencyMatrices(pconn, 'all');
+[ nmat, nlab ] = feCreateAdjacencyMatrices(pconn, 'nzw');
+[ zmat, zlab ] = feCreateAdjacencyMatrices(pconn, 'nzw_clean');
+
+% combine outputs and labels into 1 matrix
+omat = cat(3, amat, nmat, zmat);
+olab = [ alab nlab zlab ];
+
+% network stats
+[ glob{1}, node{1}, nets{1} ] = fnNetworkStats(omat(:,:,2), 16);
+[ glob{2}, node{2}, nets{2} ] = fnNetworkStats(omat(:,:,10), 16);
+%[ glob{3}, node{3}, nets{3} ] = fnNetworkStats(omat(:,:,18), 16);
+
+%% save whole output
+
+save(files.output, 'pconn', 'omat', 'olab', 'files', 'glob', 'node', 'nets', '-append');
+
+% remove parallel pool
+delete(pool);
+
+% %% save what O3D needs
+% 
+% % create labels vector
+% labels = zeros(length(rois), 1);
+% for ii = 1:length(rois)
+%     labels(ii) = rois{ii}.label;
+% end
+% 
+% % write .csv files
+% dlmwrite(fullfile(files.outpath, [ files.csv '_count.csv' ]), omat(:,:,1));
+% dlmwrite(fullfile(files.outpath, [ files.csv '_density.csv' ]), omat(:,:,2));
+% dlmwrite(fullfile(files.outpath, [ files.csv '_length.csv' ]), omat(:,:,3));
+% dlmwrite(fullfile(files.outpath, [ files.csv '_labels.csv' ]), labels);
+
+end
