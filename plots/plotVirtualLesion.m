4f1118a04181897388a99efedacafbe34b023482
bcmcpher <bcmcpher@iu.edu>
2017-04-18 14:32:12 -0400

    updated plots and job scripts

--- plots/plotVirtualLesion.m
+++ plots/plotVirtualLesion.m
@@ -7,16 +7,34 @@ function [ fig ] = plotVirtualLesion(se)
 fig(1).name = 'rmse_distributions';
 fig(1).h    = figure('name', fig(1).name, 'color', 'w');
 fig(1).type = 'eps';
+
 set(fig(1).h,'Units','normalized','Position',[0.007 0.55  0.28 0.36]);
+
+% better relative x-boundary
+dat = [ se.nolesion.rmse.all se.lesion.rmse.all ];
+jitter = (min(dat) * 0.05);
+mindat = round(min(dat) - jitter);
+maxdat = round(max(dat) + jitter);
+middat = round(maxdat / 2);
+
+% better relative y-boundary
+ymax = max([max(se.lesion.hist) max(se.nolesion.hist)]);
+ymid = ymax / 2;
+yjit = ymax * 0.10;
+
 plot(se.lesion.xhist,se.lesion.hist,'-','color', [.95 .45 .1],'linewidth',2); hold on
 plot(se.nolesion.xhist,se.nolesion.hist,'-','linewidth',2, 'color', [.1 .45 .95])
-plot([se.nolesion.rmse.mean,se.nolesion.rmse.mean], [0,0.12],'-','color',[.1 .45 .95] )
-plot([se.lesion.rmse.mean,se.lesion.rmse.mean], [0,0.12], '-', 'color',[.95 .45 .1])
+plot([se.nolesion.rmse.mean,se.nolesion.rmse.mean], [0, ymax+yjit],'-','color',[.1 .45 .95] )
+plot([se.lesion.rmse.mean,se.lesion.rmse.mean], [0, ymax+yjit], '-', 'color',[.95 .45 .1])
+
 title(sprintf('mean RMSE\nno-lesion %2.3f | lesion %2.2f',se.nolesion.rmse.mean,se.lesion.rmse.mean),'fontsize',16)
 ylabel('Probability', 'fontsize',14);xlabel('RMSE', 'fontsize',14)
+
 legend({'Lesion','No lesion'},'fontsize',14);
-set(gca,'box','off','xtick',[0 round(se.xrange(2)/2) se.xrange(2)],'ytick',[0 .06 .12],'xlim',[0 se.xrange(2)],'ylim',[0 .125], ...
-    'tickdir', 'out', 'ticklength', [0.025 0])
+
+set(gca,'box','off','xtick',[mindat round(se.xrange(2)/2) round(se.xrange(2))],'ytick',[0 ymid ymax],...
+    'xlim', [mindat maxdat], 'ylim', [0 ymax+yjit], ...
+    'tickdir', 'out', 'ticklength', [0.025 0]);
 
 %% bootstraps from strength of evidence
 
--- scripts/fnBuildEnsembleShen268Networks.m
+++ scripts/fnBuildEnsembleShen268Networks.m
@@ -1,4 +1,4 @@
-function [ pconn, rois, omat, olab, files ] = fnBuildShen268Networks(subj, track, lmax, rep, nclust, cacheDir)
+function [ pconn, rois, omat, olab, files ] = fnBuildEnsembleShen268Networks(subj, ens, nclust, cacheDir)
 % fnBuildShen268Networks creates all the data I need for the paper from a fe structure.
 %   This will probably get renamed because this is a short, simple process
 %   to run that is canonical with many pipelines.
@@ -18,54 +18,25 @@ function [ pconn, rois, omat, olab, files ] = fnBuildShen268Networks(subj, track
 
 %% build paths
 
-% build file names if tensor is called
-if strcmp(track, {'tensor_'})
-    files.fe = ['fe_structure_' subj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
-    files.out     = ['fn_' subj '_Shen268_' track '_rep' rep '.mat'];
-    files.csv     = ['fn_' subj '_Shen268_' track '_rep' rep ];
-else
-    files.fe = ['fe_structure_' subj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
-    files.out     = ['fn_' subj '_Shen268_' track '_lmax' lmax '_rep' rep '.mat'];
-    files.csv     = ['fn_' subj '_Shen268_' track '_lmax' lmax '_rep' rep ];
-end
+% built less awful names for fe structures - no conditional names
+files.fe  = ['fe_' subj '_ensemble_' ens '_REP01_500000.mat'];
+files.out = ['fn_' subj '_Shen268_ensemble_' ens '_rep01.mat'];
 
 % build paths files to save
-files.fedir  = '/N/dc2/projects/lifebid/code/ccaiafa/Caiafa_Pestilli_paper2015/Revision_Feb2017/Results/Single_TC/fe_structures';
+%files.fedir  = '/N/dc2/projects/lifebid/lifeconn';
 files.path    = '/N/dc2/projects/lifebid/lifeconn';
+files.fedir   = fullfile(files.path, 'subjects', subj, 'fibers');
 files.favol   = fullfile(files.path, 'subjects', subj, 'micro', 'fa.nii.gz');
 files.outpath = fullfile(files.path, 'subjects', subj, 'networks');
 files.output  = fullfile(files.outpath, files.out);
 files.labels  = fullfile(files.path, 'subjects', subj, 'anat', [ subj '_shen268_inflated.nii.gz' ]);
-
-% conditionals to build subject names, because that's not annoying
-
-% data sets with funny names somewhere in the process
-
-% because these FE file names are still different by data set...
-
-STN = {'FP', 'KK', 'HT', 'MP'};
-HC7 = {'7t_108323', '7t_109123', '7t_131217', '7t_910241'};
-
-% stanford data has this extra file info after subject ID in the fe structure names, not paths
-if any(strcmp(subj, STN))
-    nsubj = [ subj '_96dirs_b2000_1p5iso' ];
-    files.fe = ['fe_structure_' nsubj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
-end
-
-% 7T subjects are 7t_*, except in the fe structure names, where they're just the number
-if any(strcmp(subj, HC7))
-    nsubj = strrep(subj, '7t_', '');
-    files.fe = ['fe_structure_' nsubj '_STC_run01_' track '_lmax' lmax '_connNUM' rep '.mat'];
-end
-
-clear STN HC7
 
 %% load files
 
 display('Loading data...');
 
 % load fe structure
-load(fullfile(files.fedir, subj, files.fe));
+load(fullfile(files.fedir, files.fe));
 
 % extract all needed out of FE
 fg               = feGet(fe,   'fg acpc'); 
@@ -116,7 +87,7 @@ clear tmpdir OK
 [ pconn, rois ] = feCreatePairedConnections(parc, fg.fibers, fascicle_length, fascicle_weights);
 
 % workspace cleaning
-save(files.output, 'rois', '-append');
+save(files.output, 'rois');
 clear parc rois
 
 % clean networks
@@ -157,19 +128,5 @@ save(files.output, 'pconn', 'omat', 'olab', 'glob', 'node', 'nets', 'files', '-a
 
 % remove parallel pool
 delete(pool);
-
-% %% save what O3D needs
-% 
-% % create labels vector
-% labels = zeros(length(rois), 1);
-% for ii = 1:length(rois)
-%     labels(ii) = rois{ii}.label;
-% end
-% 
-% % write .csv files
-% dlmwrite(fullfile(files.outpath, [ files.csv '_count.csv' ]), omat(:,:,1));
-% dlmwrite(fullfile(files.outpath, [ files.csv '_density.csv' ]), omat(:,:,2));
-% dlmwrite(fullfile(files.outpath, [ files.csv '_length.csv' ]), omat(:,:,3));
-% dlmwrite(fullfile(files.outpath, [ files.csv '_labels.csv' ]), labels);
 
 end
